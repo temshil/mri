c1a6f884ea4476a478f0e50e69b64efcab3ea1bc
Темирлан Шиликбай <shilikbay@gmail.com>
2025-11-17 23:53:35 -0600

    Fix typo

---
+++ src/batch_fmri_part1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+find /temshil/data/processed -type f -path "*/func/*_bold.nii.gz" ! -path "*/func/raw/*" | while read -r fmri; do
+    echo "Processing: $fmri"
+    python3 /temshil/src/fmri.py --in_path "$fmri" --part 1
+done
