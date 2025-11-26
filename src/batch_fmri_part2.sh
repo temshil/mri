e413ab67cce1e2c85dbad0d68c3e84f83a3bb715
Темирлан Шиликбай <shilikbay@gmail.com>
2025-11-26 17:39:19 -0600

    Remove intermediate files

--- src/batch_fmri_part2.sh
+++ src/batch_fmri_part2.sh
@@ -2,5 +2,5 @@
 
 find /temshil/data/processed -type f -path "*/func/*_bold.nii.gz" ! -path "*/func/raw/*" | while read -r fmri; do
     echo "Processing: $fmri"
-    python3 /temshil/src/fmri.py --in_path "$fmri" --part 2
+    python3 /temshil/src/fmri.py --in_path "$fmri" --part 2 --rm
 done
